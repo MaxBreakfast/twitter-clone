347411091c37c890ba0faef63d03037e2c22f510
Max Ince <ince.max@gmail.com>
2016-03-29 22:30:28 -0700

    added timestamps and gravatars

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -77,6 +77,17 @@
 	  }
 
 	  _createClass(Main, [{
+	    key: 'formattedTweets',
+	    value: function formattedTweets(tweetsList) {
+	      var formattedList = tweetsList.map(function (tweet) {
+	        tweet.formattedDate = moment(tweet.created_at).fromNow();
+	        return tweet;
+	      });
+	      return {
+	        tweetsList: formattedList
+	      };
+	    }
+	  }, {
 	    key: 'addTweet',
 	    value: function addTweet(tweetToAdd) {
 	      var _this2 = this;
@@ -84,7 +95,7 @@
 	      $.post('/tweets', { body: tweetToAdd }).success(function (savedTweet) {
 	        var newTweetsList = _this2.state.tweetsList;
 	        newTweetsList.unshift(savedTweet);
-	        _this2.setState({ tweetsList: newTweetsList });
+	        _this2.setState(_this2.formattedTweets(newTweetsList));
 	      }).error(function (error) {
 	        return console.error(error);
 	      });
@@ -95,7 +106,7 @@
 	      var _this3 = this;
 
 	      $.ajax('/tweets').success(function (data) {
-	        return _this3.setState({ tweetsList: data });
+	        return _this3.setState(_this3.formattedTweets(data));
 	      }).error(function (error) {
 	        return console.log(error);
 	      });
@@ -283,15 +294,16 @@
 	      return React.createElement(
 	        "li",
 	        { className: "collection-item avatar" },
-	        React.createElement(
-	          "i",
-	          { className: "material-icons circle" },
-	          "person_pin"
-	        ),
+	        React.createElement("img", { className: "circle", src: this.props.gravatar }),
 	        React.createElement(
 	          "span",
 	          { className: "title" },
 	          this.props.name
+	        ),
+	        React.createElement(
+	          "time",
+	          null,
+	          this.props.formattedDate
 	        ),
 	        React.createElement(
 	          "p",
